cb64d2a260619de112d186f4c18e359c91352dfe
mattot <mattot.builder@gmail.com>
2025-03-10 13:44:02 +0800

    package: added shadcn/ui package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold">Student360</h1>
+      <Button>
+        shadcn/ui button
+      </Button>
     </main>
   );
 }
