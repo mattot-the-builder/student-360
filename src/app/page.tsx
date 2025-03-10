add4be986892d9b5cd18544c31a10868e94eb6ef
mattot <mattot.builder@gmail.com>
2025-03-10 13:55:18 +0800

    feat: added dashboard layout file and student page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold">Student360</h1>
-      <Button>
-        shadcn/ui button
-      </Button>
-    </main>
-  );
+    return (
+        <main>
+            <h1 className="text-3xl font-bold">Student360</h1>
+            <Link href="/student">
+                <Button>
+                    Student
+                </Button>
+            </Link>
+        </main>
+    );
 }
